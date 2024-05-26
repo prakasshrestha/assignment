7c4d45fdf49958d4b14e420f6ca11e8898e0a1ba
prakasshrestha <lucky_boy9138@yahoo.com>
2024-05-26 18:33:32 +0900

    assignment first

---
+++ PratinaAssing.java
@@ -0,0 +1,50 @@
+package pratina_c0929902_a1;
+
+class PratinaAssing {
+    public static void main(String[] args) {
+        // Single line comment
+
+        /*
+         * Multi-line comment
+         * Second line of the comment
+         */
+
+        /**
+         * Javadoc comment
+         * This describes the purpose of the class or method
+         */
+        // a. \n
+        System.out.println("This is a new line \nThis is another line");
+        // b. \"
+        System.out.println("This is a double quote: \"");
+        // c. \t
+        System.out.println("This is a tab \tThis is not a tab");
+        // d.\'
+        System.out.println("This is a single quote: \'");
+        // Data types
+
+        //----------------------------------------------------------------
+        // int, float, double, char, byte, short, long, String, boolean
+        //----------------------------------------------------------------
+        int intValue = 10;
+        float floatValue = 10.5f;
+        double doubleValue = 10.123456789;
+        char charValue = 'A';
+        byte byteValue = 100;
+        short shortValue = 1000;
+        long longValue = 1000000000L;
+        String stringValue = "Hello, world!";
+        boolean booleanValue = true;
+
+        // Printing values for each datatype
+        System.out.println("int: " + intValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("char: " + charValue);
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("long: " + longValue);
+        System.out.println("String: " + stringValue);
+        System.out.println("boolean: " + booleanValue);
+    }
+}
